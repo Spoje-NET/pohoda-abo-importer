b359032a6077387d6fbfb01cbacc0b12a7a98871
CyberVitexus <info@vitexsoftware.cz>
2026-03-03 23:31:36 +0100

    Migrace na Debian PHP autoloader systém
    - Aktualizován debian/README.Debian s instrukcemi pro Debian autoloader
    - Přechod z vendor/autoload.php na /usr/share/php/*/autoload.php
    - Využití pkg-php-tools a phpab pro generování autoloaderu
    - Dokumentace pro migraci existujících aplikací

---
+++ tests/insert-bank.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the PohodaABOimporter package
+ *
+ * https://github.com/Spoje-NET/pohoda-abo-importer
+ *
+ * (c) Spoje.Net IT s.r.o. <https://spojenet.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace mServer;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+\Ease\Shared::init(['POHODA_URL', 'POHODA_USERNAME', 'POHODA_PASSWORD'], \dirname(__DIR__).'/.env');
+
+$bankRecord = [
+    //    "MOSS" => ['ids' => '213'],
+    'account' => 'KB',
+    //    "accounting",
+    //    "accountingPeriodMOSS",
+    //    "activity" => 'testing',
+    'bankType' => 'receipt',
+    //    "centre",
+    //    "classificationKVDPH",
+    //    "classificationVAT",
+    'contract' => 'n/a',
+    'datePayment' => date('Y-m-d'),
+    'dateStatement' => date('Y-m-d'),
+    //    "evidentiaryResourcesMOSS",
+    'intNote' => 'Import works well',
+    //    "myIdentity",
+    'note' => 'Automated import',
+    'partnerIdentity' => ['address' => ['street' => 'dlouha'], 'shipToAddress' => ['street' => 'kratka']],
+    'paymentAccount' => ['accountNo' => '1234', 'bankCode' => '5500'],
+    'statementNumber' => [
+        'statementNumber' => (string) time(),
+        // 'numberMovement' => (string) time()
+    ],
+    //    "symConst" => 'XX',
+    // ?"symPar",
+    'symSpec' => '23',
+    'symVar' => (string) time(),
+    'text' => 'Testing income '.time(),
+    'homeCurrency' => ['priceNone' => '1001'],
+];
+
+//        $resolver->setAllowedValues('bankType', ['receipt', 'expense']);
+//        $resolver->setNormalizer('symVar', $resolver->getNormalizer('string20'));
+//        $resolver->setNormalizer('dateStatement', $resolver->getNormalizer('date'));
+//        $resolver->setNormalizer('datePayment', $resolver->getNormalizer('date'));
+//        $resolver->setNormalizer('text', $resolver->getNormalizer('string96'));
+//        $resolver->setNormalizer('symConst', $resolver->getNormalizer('string4'));
+//        $resolver->setNormalizer('symSpec', $resolver->getNormalizer('string16'));
+//        $resolver->setNormalizer('symPar', $resolver->getNormalizer('string20'));
+//        $resolver->setNormalizer('accountingPeriodMOSS', $resolver->getNormalizer('string7'));
+
+$banker = new Bank($bankRecord);
+$banker->addToPohoda();
+
+if ($banker->commit()) {
+    print_r($banker->response->producedDetails);
+}
